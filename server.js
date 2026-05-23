ac2deb7a443804d38d34649170a7023be68f700c
Absar Ali <absar@gmail.com>
2026-05-23 18:14:21 +0500

    port fix

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+// 🔥 IMPORTANT (Railway ke liye)
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World 🚀');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
