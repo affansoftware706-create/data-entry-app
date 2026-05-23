2fa7d2324fc1a91d6d27438684fd428665ab3ad2
Absar Ali <absar@gmail.com>
2026-05-23 18:33:05 +0500

    fix crash issue

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
